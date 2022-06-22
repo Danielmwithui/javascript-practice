bb11a971db3c931517bca0a02292908b604ec4c3
Daniel Mwithui <danielmwithui9@gmail.com>
2022-06-22 14:41:23 +0300

    First commit

---
+++ index.js
@@ -0,0 +1,12 @@
+        //This is how we write comments in Js
+       // console.log('HelloWorld');
+
+       //Declaring variables in js
+       let name= 'Daniel';
+         console.log(name);
+         function display()
+         {
+             document.getElementById("display").innerHTML="SCRIPT INCLUDED IN THE  external file"
+         }
+
+
